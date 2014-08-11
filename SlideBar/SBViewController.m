daa11b71fcb3adf144494adc85f9fd4ce2203ef3
Rudy Jahchan <rudy@carbonfive.com>
2014-08-10 22:27:26 -0700

    Initial Commit. Multiple Container Views, UIView based animation

--- SlideBar/SBViewController.m
+++ SlideBar/SBViewController.m
@@ -10,6 +10,10 @@
 
 @interface SBViewController ()
 
+@property (nonatomic) NSUInteger lastSelectedSegmentIndex;
+
+- (IBAction)didSelectSegment:(id)sender;
+
 @end
 
 @implementation SBViewController
@@ -17,13 +21,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.lastSelectedSegmentIndex = 0;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)didSelectSegment:(id)sender {
+    UISegmentedControl *segmentControl = (UISegmentedControl*)sender;
+    UIView *fromView = self.view.subviews[self.lastSelectedSegmentIndex];
+    UIView *toView = self.view.subviews[segmentControl.selectedSegmentIndex];
+    [UIView transitionFromView:fromView
+                        toView:toView
+                      duration:0.5
+                       options:(UIViewAnimationOptionShowHideTransitionViews|UIViewAnimationOptionTransitionFlipFromLeft) completion:^(BOOL finished) {
+                           if (finished) {
+                               self.lastSelectedSegmentIndex = segmentControl.selectedSegmentIndex;
+                           }
+                       }];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    NSLog(@"%@ %@ %@", NSStringFromSelector(_cmd), segue, sender);
 }
 
 @end
